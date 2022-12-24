a2197a082b5dd404cfa9d2bfb9ca9bb70dc4b84c
Youngbin Ko <youngbin.ko@student.hpi.uni-potsdam.de>
2022-12-24 12:18:47 +0100

    add messages

--- pages/cooking/home.js
+++ pages/cooking/home.js
@@ -8,8 +8,15 @@ export default function CookingHome() {
     <Grid.Container gap={2} justify="center">
       <Grid sm={12}>
         <h1>
-          Our Recipes
+          Merry Christmas🎅🤶🏽
         </h1>
+      </Grid>
+      <Grid sm={12}>
+        <h3>
+          Here's hoping the year ahead is filled with love, joy and good memories!💕 
+          <br />
+          I want to share some of our favorite meals so that you can also enjoy them. I'm still working on the recipes and they will be available soon. 🫶 
+        </h3>
       </Grid>
       <Grid xs={12} sm={4}>
         <RecipeCard />
