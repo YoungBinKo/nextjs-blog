5f92c82fe4a6b48a46ddf3c111e9a3f024ad0da4
Youngbin Ko <youngbin.ko@student.hpi.uni-potsdam.de>
2022-12-31 00:51:10 +0100

    modify navigation bar

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,13 @@ import { Navbar, Link, Button} from "@nextui-org/react";
 function MyApp({ Component, pageProps, router }) {
   return (
     <div>
-      <Navbar isBordered shouldHideOnScroll="true" variant="sticky">
+      <Navbar isCompact isBordered variant="sticky">
         <Navbar.Content enableCursorHighlight activeColor="success" variant="highlight">
           <Navbar.Link href="/">Home</Navbar.Link>
           <Navbar.Link href="/cooking/home">Cooking</Navbar.Link>
         </Navbar.Content>
     </Navbar>
-    <motion.div key={router.route}  initial="pageInitial" animate="pageAnimate" variants={{
+    <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" variants={{
       pageInitial: {
         opacity: 0
       },
