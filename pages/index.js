ec909abb69bc05baf2e11768d85a7261186b17df
Youngbin Ko <youngbin.ko@student.hpi.uni-potsdam.de>
2022-12-31 13:02:30 +0100

    modify first page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { Link, Text } from "@nextui-org/react";
 
 export default function Home() {
   return (
@@ -8,9 +9,16 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hi! This is my website for my recipes. Eventually I will add more cool things :D</p>
-        
+      <section className={utilStyles.headingLg}>
+        <Text css={{ marginBottom:"$3" }}>Hi! This is my website and recipe application.</Text>
+        <Text>I'm originally from South Korea and moved to Berlin in 2016. </Text>
+        <Text css={{ marginBottom:"$3" }}>Currently, I'm studying IT-Systems Engineering at the Hasso Plattner Institute and will graduate in April 2023.</Text>
+        <Text>
+          Feel free to reach out to me at{' '} 
+          <Link href='mailto:youngbinkoko@gmail.com'>
+            youngbinkoko@gmail.com
+          </Link>
+        </Text>
       </section>
     </Layout>
   );
